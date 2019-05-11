45e0d52d28fcdf3324f1599ad94bc743470d5fa4
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-10 19:54:41 -0500

    JAWSDB connected theoretically

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 var mysql = require("mysql");
 var pass = "toor";
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
     host: "localhost",
-  port: 3306,
-  user: "root",
-  password: pass,
-  database: "burgers_db"
-});
+    port: 3306,
+    user: "root",
+    password: pass,
+    database: "burgers_db"
+  });
+}
 
-connection.connect(function(err){
-    if (err) {
-        console.log("error connecting "+err.stack);
-        return;
-    }
-    console.log("Connected...");
+connection.connect(function (err) {
+  if (err) {
+    console.log("error connecting " + err.stack);
+    return;
+  }
+  console.log("Connected...");
 });
 
 module.exports = connection;
